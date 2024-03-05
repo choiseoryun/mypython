15b17f3daedbef7613c16f63b8ab7e113421c940
choiseoryun <seoryun03@naver.com>
2024-03-05 11:31:40 +0900

    두번째 커밋

---
+++ main.py
@@ -0,0 +1,57 @@
+from typing import Union
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from dotenv import load_dotenv
+import os
+import pymysql
+
+load_dotenv()
+
+app = FastAPI()
+
+def get_db_connection():
+    return pymysql.connect(
+        host=os.environ.get('DB_HOST'),
+        user=os.environ.get('DB_USER'),
+        password=os.environ.get('DB_PW'),
+        database=os.environ.get('DB_NAME')
+    )
+
+def get_db_cursor(db):
+    return db.cursor()
+
+class Item(BaseModel):
+    name: str
+    phonenum: str
+
+@app.post("/")
+def insert_data(item: Item):
+    db = get_db_connection()
+    cursor = get_db_cursor(db)
+    try:
+        sql = "INSERT INTO test_table (name, phonenum) VALUES (%s, %s)"
+        cursor.execute(sql, (item.name, item.phonenum))
+        db.commit()
+        return {"message": f"데이터가 성공적으로 삽입되었습니다. 이름: {item.name}, 값: {item.phonenum}"}
+    except Exception as e:
+        print(f"Error in insert_data: {e}")
+        raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        cursor.close()
+        db.close()
+
+@app.delete("/")
+def delete_data(item: Item):
+    db = get_db_connection()
+    cursor = get_db_cursor(db)
+    try:
+        sql = "DELETE FROM test_table WHERE name = %s"
+        cursor.execute(sql, (item.name,))
+        db.commit()
+        return {"message": f"데이터가 성공적으로 삭제되었습니다. 이름: {item.name}"}
+    except Exception as e:
+        print(f"Error in delete_data: {e}")
+        raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        cursor.close()
+        db.close()
